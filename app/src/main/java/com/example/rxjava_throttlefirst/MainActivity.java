8a9696b64a5667a86c4e21b62eca2220b45a119f
armanziaei <armanziaei83@gmaio.com>
2021-03-12 23:56:18 +0330

    Feat RxView : the RxView method added, throttleFirst command used

--- app/src/main/java/com/example/rxjava_throttlefirst/MainActivity.java
+++ app/src/main/java/com/example/rxjava_throttlefirst/MainActivity.java
@@ -3,12 +3,60 @@ package com.example.rxjava_throttlefirst;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.jakewharton.rxbinding4.view.RxView;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+import kotlin.Unit;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    private static final long firstReq = System.currentTimeMillis();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.btn);
+
+        RxView.clicks(button)
+                .throttleFirst(4000, TimeUnit.MILLISECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Unit>() {
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) {
+                        makeSout("<--- Subscribed --->");
+                    }
+
+                    @Override
+                    public void onNext(@NonNull Unit unit) {
+                        makeSout("onNext : " + String.valueOf(System.currentTimeMillis() - firstReq));
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+                        makeSout("Error : " + e.getMessage());
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        makeSout("<--- Completed --->");
+                    }
+                });
+    }
+
+    private void makeSout(String message){
+
+        System.out.println(message);
     }
 }
